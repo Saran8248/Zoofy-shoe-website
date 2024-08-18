b0748a15843cde9a6fa5910afbe1c0cb038dfd1c
saran <sksaran987@gmail.com>
2024-08-18 21:49:05 +0530

    zoffy

---
+++ Demo.js
@@ -0,0 +1,36 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n - 1);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-slide")[0].children;
+    let dots = document.getElementsByClassName("dot");
+
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+}
+
+setInterval(() => {
+    moveSlide(1);
+}, 1500);
